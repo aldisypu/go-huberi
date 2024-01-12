1631d65bac46ea8eebdf353aa57e3d285ed9ae2b
aldisypu <aldisypu@gmail.com>
2024-01-12 22:09:20 +0700

    config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/aldisypu/go-huberi/internal/config"
+)
+
+func main() {
+	viperConfig := config.NewViper()
+	log := config.NewLogger(viperConfig)
+	config.NewDatabase(viperConfig, log)
+	config.NewValidator(viperConfig)
+	app := config.NewFiber(viperConfig)
+
+	webPort := viperConfig.GetInt("web.port")
+	err := app.Listen(fmt.Sprintf(":%d", webPort))
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
